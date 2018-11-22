03562c415438c3ef486b07b8fce697e06f7945a7
malvernator <james.malvern@gmail.com>
2018-11-22 21:31:35 +0000

    adds an assert comparing the string'ed _id's, tests passing

--- test/reading_test.js
+++ test/reading_test.js
@@ -12,8 +12,10 @@ describe('Reading users from the database', () => {
 
     it('finds all users with a name of James', (done) => {
         User.find({ name: 'James' })
-            .then( (users) => {
+             .then( (users) => {
                 console.log(users);
+                // the _id's are ObjectIds, not raw string. need toStringing to compare them 
+                assert(users[0]._id.toString() === james._id.toString())
                 done();
             })
     })
