b1fad4ccd7c581b923ae8f1e0c8c025423a4835a
malvernator <james.malvern@gmail.com>
2018-11-22 20:41:40 +0000

    wraps the mongoose connection in a before function, adds a done() to guard against test running without connection

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-    .once('open', () => {})
-    .on('error', (error) => console.warn('Warning', error));
-    
+before( (done) => {
+    mongoose.connect('mongodb://localhost/users_test');
+    mongoose.connection
+        // uses a done() to guard against mongoose taking longer to connect
+        // everything will pause until this happens, rather than smash through the test without connecting
+        .once('open', () => { done(); })
+        .on('error', (error) => console.warn('Warning', error));    
+});
+
+
 // hook to drop collection before each test 
 beforeEach( (done) => {
     mongoose.connection.collections.users.drop(() => {
